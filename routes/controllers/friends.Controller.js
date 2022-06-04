d015a5f5b228a2dc08245320bc0718067c88e6a5
younchong <ungun96@naver.com>
2022-06-05 01:34:15 +0900

    feat: /friends POST 구현

--- routes/controllers/friends.Controller.js
+++ routes/controllers/friends.Controller.js
@@ -1,4 +1,5 @@
 const User = require("../../models/User");
+const admin = require("firebase-admin");
 
 exports.getFriendList = async (req, res, next) => {
   const uid = req.body.uid;
@@ -15,5 +16,29 @@ exports.getFriendList = async (req, res, next) => {
     res.json({ friendList });
   } catch (error) {
     res.status(500).send({ message: "server error" });
+  }
+};
+
+exports.addFriend = async (req, res, next) => {
+  const friendUid = req.body.friendUid;
+  const token = req.headers.authorization.split("Bearer ")[1];
+  const decodedToken = await admin.auth().verifyIdToken(token);
+  const uid = decodedToken.uid;
+
+  try {
+    const friend = await User.findOne({ uid: friendUid });
+    const user = await User.findOne({ uid });
+    const friendList = user.friends;
+
+    if (friendList.includes(friendUid)) {
+      return res.status(201).json({ message: "Already Added" });
+    }
+
+    friendList.push(friend.uid);
+    await User.findOneAndUpdate({ uid }, { $set: { friends: friendList } });
+
+    res.status(201).json({ message: "Add complete" });
+  } catch (error) {
+    res.status(400).json({ message: "User not found" });
   }
 };
